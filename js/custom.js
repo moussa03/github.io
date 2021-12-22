2f6fc489f7e1e87b7931be4df3122fa60195828a
moussa <moussaeloifi14@gmail.com>
2021-12-22 23:45:17 +0100

    finalizing header and fixing responsive bugs

--- js/custom.js
+++ js/custom.js
@@ -1,50 +1,65 @@
 // floating menu 
     $('.burger-menu').click(function(){
+        $('.floating-menu').show("slow");
+        // $('.call_to_action').css({'display':'none!important'});
+        // if(jQuery(window).width() >= 992){
+        //     $('.hide_call').hide();
+        // }
+       
         $('.floating-menu').css({
                 'animation-name' : 'float_menu',
                 'animation-duration' : '1s',
                 'animation-iteration-count' :'1',
-                'animation-fill-mode':'forwards'
+                'animation-fill-mode':'forwards',
+                'position':'fixed',
+                'right': '0',
+                'top': '0px'
+              
         });
-        $('.menu').css({'padding':'0px'});
+
+        
+        // $('.menu').css({'padding':'0px'});
+        // $('.container').css({'margin-right':'0px'});
         $('.overlay').css({'display':'block'});
-        $(this).hide("slow");
+        // $(this).delay(100).hide("slow");
+        // $(this).hide();
     });
+    
+// floating menu on pc 
+
+
+
 //float_menu_inverse(){
     $('.overlay ,.close-menu').click(function(){
         $('.overlay').css({'display':'none'});
         $('.floating-menu').css({
             'animation-name' : 'float_menu_inverse',
-                'animation-duration' : '1s',
+                'animation-duration' : '5s',
                 'animation-iteration-count' :'1',
                 'animation-fill-mode':'forwards'
             });
             $('.burger-menu').show("slow");
+             $('.container').css({'margin-right':'auto'});
+            $('.floating-menu').hide("slow");
+            $('.hide_call').show();
     })
-
-//show dropdown menu 
-
-// $('.plus-menu').click(function(){
-
-//     $(this).parent().next().slideToggle();
-
-// })
   
-    // $('.nav').hide();
-  
-  
+    /* drop down mobile */
     $('.plus-menu').on('click', function() {
-    //   $('.nav').slideToggle('600');
       $(this).parent().next().slideToggle('600')
       $(this).children(":first").toggleClass('fa-plus  fa-minus');
-        // if($(this).children(":first").hasClass('fa-plus')){
-        //     console.log($(this).children(":first"));
-        //     $(this).children(":first").css({'color':'red'});
-        // }
         $('.plus-menu').not(this).removeClass('active')
         $(this).addClass('active');
-
-
-      
-    //   console.log($(this).parent().next());
     });
+
+    /* drop down pc */
+    $('.header-menu li').hover(function(){
+       
+        var isHovered = $(this).is(":hover");
+        if (isHovered) {
+            $(this).children("ul").stop().slideDown(300);
+          } else {
+            $(this).children("ul").stop().slideUp(300);
+          }
+
+    })
